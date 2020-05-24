b4f581ce589292c43b5c8204704edd588aba3d63
Shuvayan Ghosh Dastidar <papaigd@gmail.com>
2020-05-24 12:58:06 +0530

    Update BasicBST.cpp

--- Solutions/BasicBST.cpp
+++ Solutions/BasicBST.cpp
@@ -68,5 +68,29 @@ void findPreSuc(Node* root, Node*& pre, Node*& succ, int key)
 }
 
 
+/**
+*
+* Constructing BST from preorder traversal array
+*/
+
+TreeNode* util( vector<int> pre, int &index , int mx ,int mn){
+    if ( index == pre.size() )return nullptr;
+    int val = pre[index];
+    if ( val > mx || val < mn) return nullptr;
+    TreeNode * root = new TreeNode( val);
+    index++;
+    root-> left = util( pre, index , val-1, mn);
+    root -> right = util (pre, index, mx,  val+1);
+    return root;
+}
+TreeNode* bstFromPreorder(vector<int>& preorder) {
+    if ( preorder.empty()) return NULL;
+    int p =0;
+    TreeNode* root = util( preorder , p , INT_MAX ,INT_MIN);
+    return root;
+
+}
+
+
 
 
