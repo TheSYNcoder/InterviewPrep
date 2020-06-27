a869c36b971c6d19964a1a94b9611706d6b2c0e4
Khalid Hassan Sk <52673145+khalid0007@users.noreply.github.com>
2020-06-27 20:28:47 +0530

    Update SuperEggDrop.cpp

--- Topic/Solution/SuperEggDrop.cpp
+++ Topic/Solution/SuperEggDrop.cpp
@@ -6,11 +6,10 @@ public:
         // DP represents total number of floor can be covered
         // with x moves and y eggs left, by DP[x][y]
         // DP[c][y] = 1 + DP[x - 1][y - 1] + DP[x - 1][y]
-        int DP[floor + 1][egg + 1]; // Max number of moves possible is equals to number of floors
+        int DP[floor + 1][egg + 1]; // Max moves possible is equals to number of floor
         
-        for(int i = 0; i <= floor; i++)
-            for(int j = 0; j <= egg; j++)
-                DP[i][j] = 0;
+        for(int i = 0; i <= egg; i++) DP[0][i] = 0;
+        for(int j = 0; j <= floor; j++) DP[j][0] = 0;
         
         int ans = 0;
         while(DP[ans][egg] < floor){
