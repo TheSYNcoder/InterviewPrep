f6538634c83eaf783028222567c12aae344d695b
Shuvayan Ghosh Dastidar <papaigd@gmail.com>
2020-07-01 01:03:27 +0530

    Update Graph2.cpp

--- Topic/Solution/Graph2.cpp
+++ Topic/Solution/Graph2.cpp
@@ -34,3 +34,33 @@ public:
         return ans;
     }
 };
+
+
+
+
+/*
+
+Alternate solution : Graph colouring
+
+*/
+
+
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        WHITE, GRAY, BLACK = 0, 1, 2
+        color = collections.defaultdict(int)
+
+        def dfs(node):
+            if color[node] != white:
+                return color[node] == BLACK
+
+            color[node] = GRAY
+            for nei in graph[node]:
+                if color[nei] == BLACK:
+                    continue
+                if color[nei] == GRAY or not dfs(nei):
+                    return False
+            color[node] = BLACK
+            return True
+
+        return filter(dfs, range(len(graph)))
