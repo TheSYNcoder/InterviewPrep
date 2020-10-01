f3166aa532a513294787989078f9b50e35561d92
Ayush Joshi <59890645+AyushJoshi2001@users.noreply.github.com>
2020-10-01 16:32:13 +0530

    Update CountingBits.cpp

--- Solutions/CountingBits.cpp
+++ Solutions/CountingBits.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         
-        vector<int> res;
-        int c=0, i;
+        vector<int> ans;
+        int count=0, i;
         for(int j=0; j<= num; j++){
             i = j;
             while(i>0){
-                if(i%2 == 1) c++;
+                if(i%2 == 1) count++;
                 i = i/2;
             }
-            res.push_back(c);
-            c=0;
+            ans.push_back(count);
+            count=0;
         }
         
-        return res;
+        return ans;
     }
 };
