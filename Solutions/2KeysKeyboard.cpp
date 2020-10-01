480b06b0783b1bfeeaabf60af40dbbd737f51c96
Ayush Joshi <59890645+AyushJoshi2001@users.noreply.github.com>
2020-10-01 22:12:10 +0530

    added 2 Keys Keyboard Solution

---
+++ Solutions/2KeysKeyboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minSteps(int n) {
+       int ans = 0, d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                ans += d;
+                n /= d;
+            }
+            d++;
+        }
+        return ans; 
+    }
+};
